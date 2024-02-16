48bc81fb93e009225561e3c50bb7ab7a47933781
rogerthatdev <roger.that.dev@gmail.com>
2024-02-15 17:52:07 -0800

    use a prompt template and chain. model will hallucinate an incorrect answer

--- index.js
+++ index.js
@@ -4,8 +4,24 @@
 import { ChatOpenAI } from "@langchain/openai";
 
 const chatModel = new ChatOpenAI({});
-
 const invocation = await chatModel.invoke("what is LangSmith?");
-
 console.log(invocation)
 
+// Import ChatPromptTemplate to use prompt templates
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+// create a prompt using template
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a world class technical documentation writer."],
+  ["user", "{input}"],
+]);
+
+// create a simple chain
+const chain = prompt.pipe(chatModel);
+
+// invoke the chain with a question
+const invokeChain = await chain.invoke({
+    input: "what is LangSmith?",
+});
+
+console.log(`\n\n############ Invoking via chain ############\n\n`, invokeChain)
